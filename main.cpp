5d17a171ae17f4efa1a9dacf17127a17c9d202c0
kpiechura <56960469+kpiechura@users.noreply.github.com>
2019-10-25 16:43:13 +0200

    update 2.0 (float random)

--- main.cpp
+++ main.cpp
@@ -3,21 +3,30 @@
 #include <string>
 #include <iostream>
 #include <windows.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main(){
+
+    srand(time(NULL));
 
     std::ifstream infile("RZEDNE.txt");
 
     ofstream myfile;
     myfile.open ("WYNIK.txt", ios::out | ios::app | ios::binary);
 
-    float sum = 0;
+    double sum = 0;
 
     for(std::string line; std::getline(infile, line); )
     {
-        float a, b, c;
+        //int los =(std::rand() % 11) + 0;
+
+        double los = 1.1 + static_cast <double> (rand()) /( static_cast <double> (RAND_MAX/(12.5-1.1))); //double random number
+
+        double a, b, c;
 
         if (!(std::istringstream(line) >> a >> b >> c))
         {
@@ -26,8 +35,8 @@ int main(){
         }
 
         sum = a + b + c;
-        cout << "| X: " << a << "\t" << "| Y: " << b << "\t" << "| Z: " << c << "\t" << "\t" << "SUMA: " << sum << endl << endl;
-        myfile << "X: " << a << "\t" << " Y: " << b << "\t" << " Z: " << c << "\t" << "\t" << "SUMA: " << sum << endl << endl;
+        cout << "| X: " << a << "\t" << "| Y: " << b << "\t" << "| Z: " << c << "\t" << "\t" << "| SUMA: " << sum << "\t" << "| LOS: " << los << "\t" << "| SUMA + LOS: " << sum + los << endl << endl;
+        myfile << "X: " << a << "\t" << " Y: " << b << "\t" << " Z: " << c << "\t" << "LOS: " << los << "\t" << "SUMA: " << sum << "\t" << "| SUMA + LOS: " << sum + los << endl << endl;
     }
 
     system ("PAUSE");
